33d8519246c81bdc80a6e2182ff6efb149315a42
Gokulkrishnan Harikrishnan <gharikri@purdue.edu>
2024-10-24 11:59:27 -0400

    placeholder update

--- lambdaHandlers/updateHandler.js
+++ lambdaHandlers/updateHandler.js
@@ -3,7 +3,7 @@ exports.updateHandler = async (event) => {
     console.log("Received event:", JSON.stringify(event, null, 2));
 
     // Extracting param`eters from the event
-    const name = event.name || "World";
+    const name = event.name || "test";
 
     // Simple response
     const response = {
