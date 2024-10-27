b11d01a2eca11e0a38eddfc89757e95df8b3b2b3
CptStickman <dexlamb11@gmail.com>
2024-10-27 19:53:49 -0400

    upload module test

--- lambdaHandlers/index.js
+++ lambdaHandlers/index.js
@@ -1,5 +1,10 @@
 const { updateHandler } = require("./updateHandler");
 
-exports.handler =async function name(event) {
-    return(updateHandler(event));
+exports.handler =async function name(event) {  //Basic handler function, serves as a Lambda functions main.
+    if (event.funct == 1){
+        return(uploadHandler(event));
+    }
+    else{
+        return(updateHandler(event));
+    }
 }
