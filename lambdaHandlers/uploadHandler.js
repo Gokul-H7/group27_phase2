ad15ef4d5e388b6ebb0bcf3930a0025725727f44
Gokulkrishnan Harikrishnan <gharikri@purdue.edu>
2024-11-14 13:44:31 -0500

    upload

--- lambdaHandlers/uploadHandler.js
+++ lambdaHandlers/uploadHandler.js
@@ -33,7 +33,7 @@ exports.handler = async (event) => {
 
     try {
         // Retrieve GitHub token from Secrets Manager
-        const githubToken = await getSecret('GITHUB_TOKEN');
+        const githubToken = await getSecret('GITHUB_TOKEN_2');
 
         // Stream the GitHub package directly to S3
         const s3Response = await streamToS3(githubLink, githubToken, s3BucketName, s3Key);
