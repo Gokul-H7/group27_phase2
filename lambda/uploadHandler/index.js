a6756215b60d7cfa1e08260dd6d180cc7e367af3
Gokulkrishnan Harikrishnan <gharikri@purdue.edu>
2024-11-24 21:10:31 -0500

    test upload

--- lambda/uploadHandler/index.js
+++ lambda/uploadHandler/index.js
@@ -1,5 +1,5 @@
 export const handler = async (event) => {
-    // github test
+    // github test 2
     const response = {
       statusCode: 200,
       body: JSON.stringify('Hello from Lambda!'),
