55c65076b8b42f8b7662ec8321811f552edb8b61
Gokulkrishnan Harikrishnan <gharikri@purdue.edu>
2024-12-01 04:21:35 -0500

    packages init

---
+++ lambda/packagesHandler/index.js
@@ -0,0 +1,9 @@
+export const handler = async (event) => {
+    // from github
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
+  };
+  
